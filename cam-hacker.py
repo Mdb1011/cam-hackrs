190bdbe4735d431e533a69689219235a9fe46ecd
Mdb1011 <89591649+Mdb1011@users.noreply.github.com>
2021-09-11 19:09:03 +0430

    Add files via upload

---
+++ cam-hacker.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+#github.coM
+#MOHAMMAD CODER
+import requests, re , colorama
+colorama.init()
+print("""
+\033[1;31m\033[1;37m ██████╗ █████╗ ███╗   ███╗      ██╗  ██╗ █████╗  ██████╗██╗  ██╗███████╗██████╗ ███████╗
+██╔════╝██╔══██╗████╗ ████║      ██║  ██║██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗██╔════╝
+██║     ███████║██╔████╔██║█████╗███████║███████║██║     █████╔╝ █████╗  ██████╔╝███████╗
+██║     ██╔══██║██║╚██╔╝██║╚════╝██╔══██║██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗╚════██║
+╚██████╗██║  ██║██║ ╚═╝ ██║      ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║███████║
+\033[1;31m ╚═════╝╚═╝  ╚═╝╚═╝     ╚═╝      ╚═╝   ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝
+\033[1;31m                                                                       
+echo"creator MOHAMMAD CODER"   
+echo"RUBIKA : Tork_apk"                                                                   
+\033[1;31m1) \033[1;37mUnited States                
+\033[1;31m31) \033[1;37mMexico                
+\033[1;31m61) \033[1;37mMoldova
+\033[1;31m2) \033[1;37mJapan                        
+\033[1;31m32) \033[1;37mFinland              
+\033[1;31m62) \033[1;37mNicaragua
+\033[1;31m3) \033[1;37mItaly                        
+\033[1;31m33) \033[1;37mChina                
+\033[1;31m63) \033[1;37mMalta
+\033[1;31m4) \033[1;37mKorea                       
+\033[1;31m34) \033[1;37mChile                 
+\033[1;31m64) \033[1;37mTrinidad And Tobago
+\033[1;31m5) \033[1;37mFrance                       
+\033[1;31m35) \033[1;37mSouth Africa          
+\033[1;31m65) \033[1;37mSoudi Arabia
+
+""")
+
+try:
+    print()
+    countries = ["US", "JP", "IT", "KR", "FR", "DE", "TW", "RU", "GB", "NL",
+                 "CZ", "TR", "AT", "CH", "ES", "CA", "SE", "IL", "PL", "IR",
+                 "NO", "RO", "IN", "VN", "BE", "BR", "BG", "ID", "DK", "AR",
+                 "MX", "FI", "CN", "CL", "ZA", "SK", "HU", "IE", "EG", "TH",
+                 "UA", "RS", "HK", "GR", "PT", "LV", "SG", "IS", "MY", "CO",
+                 "TN", "EE", "DO", "SI", "EC", "LT", "PS", "NZ", "BD", "PA",
+                 "MD", "NI", "MT", "IT", "SA", "HR", "CY", "PK", "AE", "KZ",
+                 "KW", "VE", "GE", "ME", "SV", "LU", "CW", "PR", "CR", "BY",
+                 "AL", "LI", "BA", "PY", "PH", "FO", "GT", "NP", "PE", "UY",
+                 "-"]
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux i686; rv:68.0) Gecko/20100101 Firefox/68.0"}
+
+    num = int(input("OPTIONS : "))
+    if num not in range(1, 91+1):
+        raise IndexError
+
+    country = countries[num-1]
+    res = requests.get(
+        f"https://www.insecam.org/en/bycountry/{country}", headers=headers
+    )
+    last_page = re.findall(r'pagenavigator\("\?page=", (\d+)', res.text)[0]
+
+    for page in range(int(last_page)):
+        res = requests.get(
+            f"https://www.insecam.org/en/bycountry/{country}/?page={page}",
+            headers=headers
+        )
+        find_ip = re.findall(r"http://\d+.\d+.\d+.\d+:\d+", res.text)
+        for ip in find_ip:
+            print("\033[1;31m", ip)
+except:
+    pass
+finally:
+    print("\033[1;37m")
+    exit()
